b5e2dc2032df96fad99a8484140bd7cf7600d8da
Addam Benson <abenson9@cnm.edu>
2022-08-23 09:36:41 -0700

    fixed css parallax

---
+++ public_html/documentation/js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  $('#contact').validate({
+    debug: true,
+    errorClass: 'alert alert-danger',
+    ErrorLabelContainer: '#output-area',
+    errorElement: 'div',
+    // input validation rules
+    // each rule starts with element name attribute
+    rules: {
+      name: {
+        required: true
+      },
+      email: {
+        email: true,
+        required: true
+      },
+      message: {
+        required: true,
+        maxlength: 2000
+      },
+      messages: {
+        name: {
+          required: 'Name is required.'
+        },
+        email: {
+          email: 'Please provide a valid email.',
+          required: 'Email is required.'
+        },
+        message: {
+          required: 'A message is required.',
+          maxlength: 'Message be 2000 characters or less.'
+        },
+      },
+      submitHandler: (form) => {
+        $('#contact').ajaxSubmit({
+          type: 'POST',
+          url: $('contact').attr('action'),
+          success: (ajaxOutput) => {
+            $('#output-area').css('display', '')
+            $('#output-area').html(ajaxOutput)
+
+            if ($('.alert-success') >= 1) {
+              $('#contact')[0].reset()
+            }
+          }
+        })
+      }
+    },
+  })
+})
